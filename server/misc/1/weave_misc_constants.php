edfaa160b8f01ecbbfad0b63f395e0ac6706ded7
Toby Elliott <telliott@mozilla.com>
2009-08-05 16:25:43 -0700

    adding the function to get the captcha html from the server

---
+++ server/misc/1/weave_misc_constants.php
@@ -0,0 +1,8 @@
+<?php
+
+#recaptcha data
+if (!defined('RECAPTCHA_PUBLIC_KEY')) { define('RECAPTCHA_PUBLIC_KEY', '6LfWcwUAAAAAABnmLyhmgddYeJGdiRlo2MWSOpAl'); }
+if (!defined('RECAPTCHA_PRIVATE_KEY')) { define('RECAPTCHA_PRIVATE_KEY', '6LfWcwUAAAAAAHpjpBNSaxwLVQXQIG-S0Y6IG38O '); }
+
+
+?>
