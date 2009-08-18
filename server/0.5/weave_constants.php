c21df3edd4671f3253c8b8e980f1df665e5e665c
Toby Elliott <telliott@mozilla.com>
2009-08-18 14:40:00 -0700

    reading the default constants in all cases. That way, the host-specific constants file just needs to define what overrides

--- server/0.5/weave_constants.php
+++ server/0.5/weave_constants.php
@@ -1,6 +1,7 @@
 <?php
 	if (file_exists($_SERVER['HTTP_HOST'] . '_constants.php'))
 		require_once $_SERVER['HTTP_HOST'] . '_constants.php';
-	else
+
+	if (file_exists('default_constants.php'))
 		require_once 'default_constants.php';
 ?>
