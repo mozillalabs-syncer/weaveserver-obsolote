2bb17ee43a220788d19804473acfcdd1e32ed247
Toby Elliott <telliott@mozilla.com>
2009-11-13 15:56:26 -0800

    more hacking at the utf8

--- server/user/1/index.php
+++ server/user/1/index.php
@@ -158,7 +158,7 @@
 			$jsonstring = '';
 			while ($data = fread($putdata,2048)) {$jsonstring .= $data;}
 			fclose($putdata);
-			$json = json_decode($jsonstring, true);
+			$json = json_decode(utf8_encode($jsonstring), true);
 			if ($json === null)
 				report_problem(6, 400);
 
@@ -186,7 +186,7 @@
 				report_problem(4, 400);
 
 
-			$password = utf8_encode($json['password']);
+			$password = $json['password'];
 			$email = $json['email'];
 
 			if (!verify_password_strength($password, $url_user))
